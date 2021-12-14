61b11f3de112888488adb8680bce63c2f9752cf0
niieli <96092663+niieli@users.noreply.github.com>
2021-12-14 15:02:14 +0100

    New Ardisinius Ore! Textures and functions wille be reworked later on!

--- LEGGO/src/main/java/net/fabricmc/ardis/ArdisTheLegendMod.java
+++ LEGGO/src/main/java/net/fabricmc/ardis/ArdisTheLegendMod.java
@@ -3,7 +3,10 @@ package net.fabricmc.ardis;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
@@ -19,11 +22,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.List;
 
 public class ArdisTheLegendMod implements ModInitializer {
-	//TODO
-	// Ardis Food recipe/obtainable
-	// Ardisnius Ore
-	// Ardisimo Wood
-	// Ardis Rüstung & Tools
+	//TODO bis Samstag
+	// Ardis Food recipe/obtainable https://fabricmc.net/wiki/tutorial:recipes
+	// Ardisnius Ore generation + richtige Texturen https://fabricmc.net/wiki/tutorial:blocks https://fabricmc.net/wiki/tutorial:ores
+	// Ardisimo Wood https://fabricmc.net/wiki/tutorial:features https://fabricmc.net/wiki/tutorial:trees
+	// Ardis Rüstung & Tools https://fabricmc.net/wiki/tutorial:armor https://fabricmc.net/wiki/tutorial:tools https://fabricmc.net/wiki/tutorial:shield
 	//This logger is used to write text to the console and the log file.
 	//It is considered best practice to use your mod id as the logger's name.
 	//That way, it's clear which mod wrote info, warnings, and errors.
@@ -32,6 +35,12 @@ public class ArdisTheLegendMod implements ModInitializer {
 	public static final ItemGroup ARDIS_THE_LEGEND = FabricItemGroupBuilder.build(
 			new Identifier("ardis-the-legend", "general"),
 			() -> new ItemStack(ArdisTheLegendMod.ARDIS_FOOD));
+
+	// a block
+	public static final ArdisiniusOre ARDISINIUS_ORE = new ArdisiniusOre(FabricBlockSettings.of(Material.STONE)
+			.hardness(2.7f)
+			.resistance(8.0f)
+			.requiresTool());
 
 	// an instance of our new item
 	public static final ArdisFood ARDIS_FOOD = new ArdisFood(new FabricItemSettings()
@@ -56,6 +65,14 @@ public class ArdisTheLegendMod implements ModInitializer {
 			tooltip.add( new TranslatableText("item.ardis-the-legend.ardis_food.tooltip_2").formatted(Formatting.GOLD) );
 		}
 	}
+	public static class ArdisiniusOre extends Block {
+		public ArdisiniusOre(Settings settings) {
+			super(settings);
+		}
+
+
+	}
+
 
 	@Override
 	public void onInitialize() {
@@ -64,6 +81,11 @@ public class ArdisTheLegendMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		Registry.register(Registry.ITEM, new Identifier("ardis-the-legend", "ardis_food"), ARDIS_FOOD);
+		Registry.register(Registry.BLOCK, new Identifier("ardis-the-legend", "ardisinius_ore"), ARDISINIUS_ORE);
+		Registry.register(Registry.ITEM, new Identifier("ardis-the-legend", "ardisinius_ore"), new BlockItem(ARDISINIUS_ORE, new FabricItemSettings()
+				.group(ArdisTheLegendMod.ARDIS_THE_LEGEND)));
+
+
 		LOGGER.info("Hello, this is Ardis!");
 	}
 }
